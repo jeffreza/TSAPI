e66a3727c98616ebed9daab6b29600307dbb709e
jeffreza <jeffreza041992@gmail.com>
2020-03-12 22:52:53 +0500

    besty1

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 const http = require("http");
 const WebSocketServer = require("websocket").server;
-const httpServer =  http.createServer ((req,res)=> {
-    console.log("we have received a request");
-
+connection = null;
+const httpserver = http.createServer((req, res) => {
+  console.log("we have received a request");
 });
 
-const websocket = new WebSocketServer({"httpServer": httpServer});
-httpServer.listen(8080, ()=> (console.log("my server is listening")));
+const websocket = new WebSocketServer({ httpServer: httpserver });
+httpServer.listen(8080, () => console.log("my server is listening"));
+
+websocket.on("request", request =>{
+   connection = request.accept(null, request.origin);
+   connection.on("onopen", e => console.log("open connection") );
+   connection.on("onclose", e => console.log("close connection") );
+   connection.on("onmessage", e => console.log("open connection") );
+});
