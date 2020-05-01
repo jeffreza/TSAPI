24874bdf918a335c5197dd56ca5cf93a4a99a90a
jeffreza <jeffreza041992@gmail.com>
2020-05-02 01:04:53 +0500

    gigalo

--- src/hello.ts
+++ src/hello.ts
@@ -1,9 +1,32 @@
-function hello(username: string){
+this.router.post('/forgetPassword', async (req, res) => {
+    let email = req.body.email
+    let user = await new userBuss().getAuthenticated(email);
 
-    return "hello"+ username;
+    if (user == null) {
+        message: "this email is not in our database"
+    } else {
+        let consaltant: Iconsaltants = await new ConsaltantBuss().getOneConsaltantByUserId(user._id);
+        genSalt(10, (err, salt) => {
+            if (err) throw err
+            hash(email, salt, (err, hash) => {
+                if (err) throw err
+                let forget = new userBuss().forgotpasswordLinkgenration(user._id, hash);
+                let forgetLink = "http://69.16.200.12:8001/recover-password/" + hash + "|jaboo|||" + user._id
+                
+                let html = readFileSync('src/Public/mailtemp/activationCode/Forgetpassword.html', 'utf8');
+                let template = handlebars.compile(html)
+                let replacments = {
+                    username: consaltant.C_First_Name,
+                    forgetLink:forgetLink
+                }
+                let htmltosend = template(replacments)
+                let newMail = new Mail(user.email, "Password Reset Link", '',htmltosend);
+                newMail.sendMail();
+                res.status(200).json({
+                    message: "Link Send to your registerd email"
+                })
+            })
+        })
+    }
 
-}
-
-let user= "ali";
-
-console.log(hello(user));
+})
--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
-const http = require("http");
+/*const http = require("http");
 const WebSocketServer = require("websocket").server;
 connection = null;
 const httpserver = http.createServer((req, res) => {
   console.log("we have received a request");
 });
 
-const websocket = new WebSocketServer({ httpServer: httpserver });
-httpServer.listen(8080, () => console.log("my server is listening"));
+const websocket = new WebSocketServer({ httpServer:httpserver });
+httpserver.listen(8080, () => console.log("my server is listening"));
 
 websocket.on("request", request =>{
-   connection = request.accept(null, request.origin);
-   connection.on("onopen", e => console.log("open connection") );
-   connection.on("onclose", e => console.log("close connection") );
-   connection.on("onmessage", e => console.log("open connection") );
-});
+    connection = request.accept(null, request.origin);
+    connection.on("onopen", () => console.log("open connection") );
+    connection.on("onclose", () => console.log("close connection") );
+    connection.on("onmessage", messag =>
+    {console.log(`I received: ${message}`)}  );
+ });*/
