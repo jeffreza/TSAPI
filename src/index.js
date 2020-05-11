8435584d72bdabd34c1ec74086523a21a2ad709d
jeffreza <jeffreza041992@gmail.com>
2020-05-11 05:43:02 +0500

    uus

--- src/index.js
+++ src/index.js
@@ -1,17 +1,33 @@
-/*const http = require("http");
-const WebSocketServer = require("websocket").server;
-connection = null;
-const httpserver = http.createServer((req, res) => {
-  console.log("we have received a request");
+const express = require("express");
+const app = express();
+
+app.get("/isPrime", (req, res) => {
+  try {
+    const response = isPrime(parseInt(req.qurey.number));
+    res.send(response);
+  } catch (error) {
+    console.log(error);
+  }
 });
 
-const websocket = new WebSocketServer({ httpServer:httpserver });
-httpserver.listen(8080, () => console.log("my server is listening"));
+app.listen(8081, console.log("listening at port 8081"));
 
-websocket.on("request", request =>{
-    connection = request.accept(null, request.origin);
-    connection.on("onopen", () => console.log("open connection") );
-    connection.on("onclose", () => console.log("close connection") );
-    connection.on("onmessage", messag =>
-    {console.log(`I received: ${message}`)}  );
- });*/
+function isPrime(number) {
+  let startTime = new Date();
+  let endTime = new Date();
+  let isPrime = true;
+  for (let i = 3; i < number; i++) {
+    if (number % 3 === 0) {
+      endTime = new Date();
+      isPrime = false;
+      break;
+    }
+    if (isPrime) endTime = new Date();
+
+    return {
+      number: number,
+      isPrime: isPrime,
+      time: endTime.getTime() - startTime().getTime(),
+    };
+  }
+}
